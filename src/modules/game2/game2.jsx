8f7aef7a6a06950f60f4a37aa7fbd385984143dd
eivindwoien <maybonofficial@gmail.com>
2024-03-21 21:33:16 +0100

    Fixed game 1 after Vebjorn broke it. Problem was in the checkArrayMatch

--- src/modules/game2/game2.jsx
+++ src/modules/game2/game2.jsx
@@ -70,17 +70,17 @@ function Game2() {
 		console.log(active, over);
 
 		if (active.id !== over.id) {
-			setItems((items) => {
-				const oldIndex = items.indexOf(active.id);
-				const newIndex = items.indexOf(over.id);
+			const oldIndex = items.indexOf(active.id);
+			const newIndex = items.indexOf(over.id);
 
-				return arrayMove(items, oldIndex, newIndex);
-			});
-		}
-		const match = checkArrayMatch(items);
-		if (match) {
-			setSuccess(true);
-			setIsRunning(false);
+			const newArray = arrayMove(items, oldIndex, newIndex);
+			setItems(newArray);
+
+			const match = checkArrayMatch(newArray);
+			if (match) {
+				setSuccess(true);
+				setIsRunning(false);
+			}
 		}
 	};
 
